0e3925af0de6db21f76276dbc89124ec9f39dbda
Dave Madden <davelearnsstuff@gmail.com>
2021-12-02 16:44:14 -0800

    server up with home stub

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 require('dotenv').config()
+const express = require('express')
+const { reset } = require('nodemon')
+const server = express()
+
+server.use(express.json())
+
+server.get('/', (req, res)=>{
+    res.send("<h1>this is not the API you're looking for </h1>")
+})
 
 const PORT = process.env.PORT || 9000
+
+server.listen(PORT, ()=>{
+    console.log("listening on port ", PORT)
+})
