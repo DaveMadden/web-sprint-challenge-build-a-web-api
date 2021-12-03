c327956d7a3f8a0903fc977201f9ad41cba6c276
Dave Madden <davelearnsstuff@gmail.com>
2021-12-03 12:14:37 -0800

    get endpoint up

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,11 @@
-// add middlewares here related to projects
+const Project = require('./projects-model')
+
+function validateProjectID(req, res, next){
+    console.log("validateProjID")
+}
+
+function validateProjectBody(req, res, next){
+    console.log("validate project body")
+}
+
+module.exports = { validateProjectID, validateProjectBody }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -2,10 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 //import middleware here
+const {
+    validateProjectID, 
+    validateProjectBody 
+} = require('./projects-middleware')
 
 const Project = require('./projects-model')
 
 //ENDPOINTS GO HERE
-
+router.get('/', (req, res) => {
+    Project.get()
+      .then(response =>{
+        res.status(200).json(response);
+      })
+      .catch(error =>{
+        res.status(500).json({message: `${error}`});
+      })
+  });
 
 module.exports = router;
